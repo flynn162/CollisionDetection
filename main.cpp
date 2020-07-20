7dd64f068b12549dc5779de95c246ab722fa1d17
Flynn Liu <54922775+flynn162@users.noreply.github.com>
2020-07-20 13:50:03 -0700

    Implement "Set add" and "Set delete"

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <math.h>
 #include "bptree.hpp"
 
-constexpr size_t MAX_LENGTH = 5;
+constexpr size_t NODE_DATA_SIZE = 6;
+constexpr size_t HEADER_DATA_SIZE = 5;
 
 struct Hitbox {
     // W = [a1, b1] X [a2, b2]
@@ -9,30 +11,185 @@ struct Hitbox {
 };
 
 struct alignas(64) SetNode {
-    float label;
-    Hitbox* data[MAX_LENGTH];
+    Hitbox* data[NODE_DATA_SIZE];
     SetNode* next;
     SetNode* prev;
+};
+
+struct alignas(64) SetHeader {
+    float label;
+    unsigned char length_of_last_node;
+    SetNode* first;
+    SetNode* last;
+    Hitbox* data[HEADER_DATA_SIZE];
 };
 
 struct MaybeHitbox {
     union {
         float label;
         Hitbox hb;
-        SetNode s;
+        SetHeader s;
     };
 };
 
 constexpr bool structs_are_aligned() {
     return offsetof(Hitbox, a1) == 0
-        && offsetof(SetNode, label) == 0
+        && offsetof(SetHeader, label) == 0
         && offsetof(MaybeHitbox, label) == 0
         && offsetof(MaybeHitbox, hb) == 0
         && offsetof(MaybeHitbox, s) == 0;
 }
 static_assert(structs_are_aligned());
 static_assert(struct_size_is_appropriate<SetNode>());
+static_assert(struct_size_is_appropriate<SetHeader>());
 
+
+static SetHeader* make_set_header(Hitbox* initial_element) {
+    auto result = new SetHeader();
+    result->label = NAN;
+    result->length_of_last_node = 1;
+    result->first = nullptr;
+    result->last = nullptr;
+    result->data[0] = initial_element;
+    return result;
+}
+
+static SetNode* make_set_node(Hitbox* value, SetNode* next, SetNode* prev) {
+    auto result = new SetNode();
+    result->next = next;
+    result->prev = prev;
+    result->data[0] = value;
+    return result;
+}
+
+static void delete_set_header(SetHeader* self) {
+    delete self;
+}
+
+static void delete_set_node(SetNode* self) {
+    delete self;
+}
+
+static SetNode* add(SetHeader* self, Hitbox* value) {
+    if (self->last == nullptr) {
+        if (self->length_of_last_node < HEADER_DATA_SIZE) {
+            self->data[self->length_of_last_node] = value;
+            self->length_of_last_node++;
+        } else {
+            auto new_node = make_set_node(value, nullptr, nullptr);
+            self->length_of_last_node = 1;
+            self->first = new_node;
+            self->last = new_node;
+        }
+    } else {
+        if (self->length_of_last_node < NODE_DATA_SIZE) {
+            self->last->data[self->length_of_last_node] = value;
+            self->length_of_last_node++;
+        } else {
+            auto new_node = make_set_node(value, nullptr, self->last);
+            self->last->next = new_node;
+            self->last = new_node;
+            self->length_of_last_node = 1;
+        }
+    }
+}
+
+static bool is_singleton(SetHeader* self) {
+    return self->last == nullptr && self->length_of_last_node == 1;
+}
+
+static bool is_empty(SetHeader* self) {
+    return self->last == nullptr && self->length_of_last_node == 0;
+}
+
+template<class T, class V>
+static int find_in_node(T* self, V value, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        if (self->data[i] == value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+static SetNode* find(SetHeader* self, Hitbox* value, size_t* idxout) {
+    // Find the set node and the in-node index of a value. Return a pointer to
+    // the set node that contains the value. If value is in the header, return
+    // null. The index is always written to idxout.
+    //
+    // Behavior is undefined if the value is not in the set.
+
+    size_t size = HEADER_DATA_SIZE;
+    if (self->last == nullptr)
+        size = self->length_of_last_node;
+
+    // iterate through the header first
+    int idx;
+    idx = find_in_node(self, value, size);
+    if (idx >= 0) {
+        *idxout = idx;
+        return nullptr;
+    }
+
+#ifdef DEBUG
+    if (self->last == nullptr)
+        throw std::logic_error("value not found");
+#endif
+
+    // iterate through set nodes
+    SetNode* curr = self->first;
+    while (curr->next != nullptr) {
+        idx = find_in_node(curr, value, NODE_DATA_SIZE);
+        if (idx >= 0) {
+            *idxout = idx;
+            return curr;
+        }
+    }
+    // last set node
+    idx = find_in_node(curr, value, self->length_of_last_node);
+#ifdef DEBUG
+    if (idx == -1)
+        throw std::logic_error("value not found");
+#endif
+    *idxout = idx;
+    return curr;
+}
+
+static void del(SetHeader* self, Hitbox* value) {
+    // Delete a value from the set
+    // Behavior is undefined if the value is not in the set
+
+    // find the value
+    size_t idx = 0;
+    SetNode* node = find(self, value, &idx);
+
+    // fill the gap
+    if (self->last == nullptr) {
+        // only need to modify the header
+        self->data[idx] = self->data[self->length_of_last_node - 1];
+        self->length_of_last_node--;
+    } else {
+        // move the last value into the gap
+        Hitbox* new_value = self->last->data[self->length_of_last_node - 1];
+        if (node == nullptr)
+            self->data[idx] = new_value;
+        else
+            node->data[idx] = new_value;
+        // shrink node or delete node
+        if (self->length_of_last_node == 1) {
+            auto to_be_freed = self->last;
+            self->last = self->last->prev;
+            if (self->last == nullptr)
+                self->first = nullptr;
+            else
+                self->last->next = nullptr;
+            delete_set_node(to_be_freed);
+            self->length_of_last_node = NODE_DATA_SIZE;
+        } else {
+            self->length_of_last_node--;
+        }
+    }
+}
 
 class HitboxIndex : public BPTree<Hitbox*> {
 protected:
